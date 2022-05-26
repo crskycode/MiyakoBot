6355a9c07bb9967ac6c4bcb8feb95396527a843f
Crsky <crskycode@hotmail.com>
2022-05-26 15:01:56 +0800

    Build framework

--- MiyakoBot.Http/ProxyedHttpClient.cs
+++ MiyakoBot.Http/ProxyedHttpClient.cs
@@ -1,15 +1,39 @@
-﻿using System.Text.Json.Nodes;
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json.Nodes;
+using System.Net.Http;
+using System.Net;
 
 namespace MiyakoBot.Http
 {
     public class ProxyedHttpClient : IProxyedHttpClient
     {
+        readonly IConfiguration _configuration;
         readonly BaseHttpClient _client;
 
-        public ProxyedHttpClient()
+        public ProxyedHttpClient(IConfiguration configuration)
         {
-            // TODO: Add proxy
-            _client = new BaseHttpClient(new HttpClient());
+            _configuration = configuration.GetSection(nameof(ProxyedHttpClient));
+
+            var proxyUrl = _configuration["Proxy"];
+
+            HttpClient httpClient;
+
+            if (!string.IsNullOrEmpty(proxyUrl))
+            {
+                var httpClientHandler = new HttpClientHandler
+                {
+                    Proxy = new WebProxy(proxyUrl),
+                    UseProxy = true
+                };
+
+                httpClient = new HttpClient(httpClientHandler);
+            }
+            else
+            {
+                httpClient = new HttpClient();
+            }
+
+            _client = new BaseHttpClient(httpClient);
         }
 
         public Task<byte[]> GetByteArrayAsync(Uri uri)
